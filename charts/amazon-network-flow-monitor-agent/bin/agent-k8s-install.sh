ec06d56dd00c6f541331393a58a873ddc3599f1a
riquich <34640902+riquich@users.noreply.github.com>
2025-07-24 14:09:21 +0100

    shift addon version to latest (#44)
    Co-authored-by: Ertugrul Erdem <eerdem@amazon.com>

--- charts/amazon-network-flow-monitor-agent/bin/agent-k8s-install.sh
+++ charts/amazon-network-flow-monitor-agent/bin/agent-k8s-install.sh
@@ -16,7 +16,7 @@ VALUES_FILENAME=values.yaml
 PROD_CONTAINER_REGISTRY="602401143452.dkr.ecr.us-west-2.amazonaws.com"
 export CONTAINER_REGISTRY=${CONTAINER_REGISTRY:-$PROD_CONTAINER_REGISTRY}
 
-export LATEST_KNOWN_IMAGE_TAG="v1.0.2-eksbuild.4"
+export LATEST_KNOWN_IMAGE_TAG="v1.0.3-eksbuild.1"
 export IMAGE_TAG=${IMAGE_TAG:-$LATEST_KNOWN_IMAGE_TAG}
 
 # Will install 'Amazon CloudWatch Network Flow Monitor Agent' Manifest Files to an existing K8s Cluster
